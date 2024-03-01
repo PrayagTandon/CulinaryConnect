86140ee565372865811cf4b218f0e318fb257b2c
Prayag Tandon <tandonprayag99@gmail.com>
2024-03-01 14:51:07 +0530

    ...

--- dist/index.e37f48ea.js
+++ dist/index.e37f48ea.js
@@ -591,8 +591,31 @@ const timeout = function(s) {
             reject(new Error(`Request took too long! Timeout after ${s} second`));
         }, s * 1000);
     });
-}; // https://forkify-api.herokuapp.com/v2
- ///////////////////////////////////////
+};
+// API location -> https://forkify-api.herokuapp.com/v2
+///////////////////////////////////////
+/* First API call */ const showRecipe = async function() {
+    try {
+        const res = await fetch("https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886");
+        const data = await res.json();
+        if (!res.ok) throw new Error(`${data.message} (${res.status}) - Invalid ID requested!!`);
+        let { recipe } = data.data;
+        recipe = {
+            id: recipe.id,
+            img: recipe.image_url,
+            cookingTime: recipe.cooking_time,
+            publisher: recipe.publisher,
+            servings: recipe.servings,
+            sourceURL: recipe.source_url,
+            title: recipe.title,
+            ingredients: recipe.ingredients
+        };
+        console.log(recipe);
+    } catch (err) {
+        alert(err);
+    }
+};
+showRecipe();
 
 },{}]},["hycaY","aenu9"], "aenu9", "parcelRequire37b9")
 
--- src/js/controller.js
+++ src/js/controller.js
@@ -8,7 +8,37 @@ const timeout = function (s) {
   });
 };
 
-// https://forkify-api.herokuapp.com/v2
+// API location -> https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
 
+/* First API call */
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
+
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} (${res.status}) - Invalid ID requested!!`);
+
+    let { recipe } = data.data;
+
+    recipe = {
+      id: recipe.id,
+      img: recipe.image_url,
+      cookingTime: recipe.cooking_time,
+      publisher: recipe.publisher,
+      servings: recipe.servings,
+      sourceURL: recipe.source_url,
+      title: recipe.title,
+      ingredients: recipe.ingredients,
+    }
+
+    console.log(recipe);
+  }
+  catch (err) {
+    alert(err)
+  }
+}
+showRecipe();
