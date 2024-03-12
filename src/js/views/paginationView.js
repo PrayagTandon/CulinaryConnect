cab0ad85540c7c03f9fa4d7fce8d1f5947f07173
Prayag Tandon <tandonprayag99@gmail.com>
2024-03-12 11:32:35 +0530

    Pagination  button markup

--- src/js/views/paginationView.js
+++ src/js/views/paginationView.js
@@ -5,26 +5,54 @@ class PaginationView extends View {
     _parentElement = document.querySelector('.pagination');
 
     _generateMarkup() {
+        const currPage = this._data.page;
         const numPages = Math.ceil(this._data.results.length / this._data.resultsPerPage);
         console.log(numPages);
 
         // At Page 1 and there are other pages
-        if (this._data.page === 1 && numPages > 1) {
-            return `Page 1, Other page available`;
+        if (currPage === 1 && numPages > 1) {
+            return `
+                <button class="btn--inline pagination__btn--next">
+                    <span>Page ${currPage + 1}</span>
+                    <svg class="search__icon">
+                        <use href="${icons}#icon-arrow-right"></use>
+                    </svg>
+                </button>
+            `;
         };
 
         // Last Page
-        if (this._data.page === numPages && numPages > 1) {
-            return `Last Page`
+        if (currPage === numPages && numPages > 1) {
+            return `
+                <button class="btn--inline pagination__btn--prev">
+                    <svg class="search__icon">
+                        <use href="${icons}#icon-arrow-left"></use>
+                    </svg>
+                    <span>Page ${currPage - 1}</span>
+                </button>
+            `;
         };
 
         // Any other page
-        if (this._data.page !== 1 && this._data.page < numPages) {
-            return `Any other page`;
+        if (currPage !== 1 && this._data.page < numPages) {
+            return `
+                <button class="btn--inline pagination__btn--prev">
+                    <svg class="search__icon">
+                        <use href="${icons}#icon-arrow-left"></use>
+                    </svg>
+                    <span>Page ${currPage - 1}</span>
+                </button>
+                <button class="btn--inline pagination__btn--next">
+                    <span>Page ${currPage + 1}</span>
+                    <svg class="search__icon">
+                        <use href="${icons}#icon-arrow-right"></use>
+                    </svg>
+                </button>
+            `;
         };
 
         // At Page 1 and there are NO other pages
-        return `No other page`;
+        return ``;
     }
 };
 
