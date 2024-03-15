9728d4c2052d61144a1f936547f977bef1b945f8
Prayag Tandon <tandonprayag99@gmail.com>
2024-03-15 12:10:50 +0530

    Adding Recipe Section

---
+++ src/js/views/addRecipeView.js
@@ -0,0 +1,13 @@
+import View from "./view.js";
+import icons from '../../img/icons.svg';
+
+class AddRecipeView extends View {
+    _parentElement = document.querySelector('.pagination');
+
+    _generateMarkup() {
+
+    }
+
+};
+
+export default new AddRecipeView();
